afbf3f0ec5a72de153b87e8f44885f7c9b22cd21
rahuls3281 <rahuls3281@gmail.com>
2024-09-21 23:03:02 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"message":"This is Home API"}
+
+@app.route("/testing")
+def testing():
+    return {"message":"This is testing API"}
+
+app.run()
